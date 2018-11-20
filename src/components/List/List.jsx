bf25d4a4054aac3d954e8337b22860daa2a8825b
Sarabouch <sara93bo@hotmail.com>
2018-11-20 11:22:14 +0100

    episode component done

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -8,14 +8,33 @@ const EpisodeList = () => {
         <div className="episodeList">
             <div className="episodecontainer">
                 <div className="episod-number">
-                    <img src="/ee0e70b2ae91f7209f3a78247986e280--disney-princes-disney-cruiseplan.jpg" alt="" />
+                    <img className="episode-img" src="/undraw_interview_rmcf.svg" alt="" />
                 </div>
                 <div className="episode">
                     <h5>episode #1</h5>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, inventore!</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus,
+                        inventore! ipsum dolor sit amet consectetur adipisicing elit. Accusamus, inventore!</p>
                     <div className="guest">
                         <img src="/ee0e70b2ae91f7209f3a78247986e280--disney-princes-disney-cruiseplan.jpg" alt="" />
-                        <h4>Guest: hjhjh from sfdfdf</h4>
+                        <h4> <span> Guest:</span> gsghhsd gdsg from <a href="/">jkjk</a></h4>
+                    </div>
+                    <Button>Listen on Soundcloud  →</Button>
+                    <a href="/">or jump to iTunes</a>
+                </div>
+
+            </div>
+
+            <div className="episodecontainer">
+                <div className="episod-number">
+                    <img className="episode-img" src="/undraw_interview_rmcf.svg" alt="" />
+                </div>
+                <div className="episode">
+                    <h5>episode #1</h5>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus,
+                        inventore! ipsum dolor sit amet consectetur adipisicing elit. Accusamus, inventore!</p>
+                    <div className="guest">
+                        <img src="/ee0e70b2ae91f7209f3a78247986e280--disney-princes-disney-cruiseplan.jpg" alt="" />
+                        <h4> <span> Guest:</span> gsghhsd gdsg from <a href="/">jkjk</a></h4>
                     </div>
                     <Button>Listen on Soundcloud  →</Button>
                     <a href="/">or jump to iTunes</a>
